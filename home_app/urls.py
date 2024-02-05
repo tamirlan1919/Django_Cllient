a80f8cf8f11be3d4c70200adbdbf67a7f228d025
tamirlan1919 <tchinchaev@bk.ru>
2024-02-05 10:09:15 +0300

    init

---
+++ home_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('products/', views.get_all_products, name='products'),
+    path('change_product/<int:product_id>/', views.change_product, name='change_product'),
+]
---
+++ home_app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
+from home_app.forms import ImageForm
+from . import models
+from . import forms
+
+
+def index(request):
+    return render(request, 'home_app/base1.html')
+
+
+def get_all_products(request):
+    products = models.Product.objects.all()
+    return render(request, 'home_app/products.html', {'products': products})
+
+
+def change_product(request, product_id):
+    product = models.Product.objects.filter(pk=product_id).first()
+    form = forms.ProductForm(request.POST, request.FILES)
+    if request.method == 'POST' and form.is_valid():
+        image = form.cleaned_data['image']
+        if isinstance(image, bool):
+            image = None
+        if image is not None:
+            fs = FileSystemStorage()
+            fs.save(image.name, image)
+        product.name = form.cleaned_data['name']
+        product.description = form.cleaned_data['description']
+        product.price = form.cleaned_data['price']
+        product.amount = form.cleaned_data['amount']
+        product.image = image
+        product.save()
+        return redirect('products')
+    else:
+        form = forms.ProductForm(initial={'name': product.name, 'description': product.description,
+                                          'price': product.price, 'amount': product.amount, 'image': product.image})
+
+    return render(request, 'home_app/change_product.html', {'form': form})
+
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES) # request.POST чтобы получить текстовую информацию , request.FILES чтобы получить байты
+        if form.is_valid():
+            image = form.cleaned_data['image']
+            fs = FileSystemStorage()  # FileSystemStorage экземпляр позволяет работать с файлами
+            fs.save(image.name, image)
+    else:
+        form = ImageForm()
+    return render(request, 'home_app/upload_image.html', {'form': form})
